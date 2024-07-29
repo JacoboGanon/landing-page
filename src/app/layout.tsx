86d39dc551605c6be54ab04fbf06cb6e4643a15d
Jacob Dell <22jacganon@gmail.com>
2024-07-28 19:35:26 -0700

    First commit to push everything

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Jacobo Ganon',
+    default: 'Jacobo Ganon - Software Engineer, and Data Scientist',
+  },
+  description:
+    "I'm Jacob, a software engineer and data scientist, based in Arizona. I specialize in building and deploying machine learning models, and web applications.",
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
